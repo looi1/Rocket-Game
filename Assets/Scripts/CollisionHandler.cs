c1ebc2f55b5dc06ece636e2219b9b2e0b9331ea7
looi1 <tecksan394@gmail.com>
2024-05-17 15:54:55 +0800

    added refined rocket, completed level 1, added lights, added sound effect, added particles and added obstacles

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -5,52 +5,98 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour   
 {
+    [SerializeField] AudioClip crashAudio;
+    [SerializeField] AudioClip successAudio;
+    [SerializeField] ParticleSystem crashParticles;
+    [SerializeField] ParticleSystem successParticles;
+
+    AudioSource audioData;
     int health = 3;
     float delay = 2f;
     bool won = false;
+    bool isTransitioning = false;
+    bool crashToggle = false;
+
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        DebugKeys();
+    }
+
     void OnCollisionEnter(Collision collision)
     {
-        switch (collision.gameObject.tag)
+        if (!isTransitioning)
         {
-            case "Friendly":
-                Debug.Log("you're good");
-                break;
-            case "Fuel":
-                Debug.Log("Topped up fuel !");
-                break;
-            case "Finish":
-                Debug.Log("Congrats you won !");
-                WinEffect();
-                
-                break;
-            default:
-                if (!won) { 
-                    health -= 1;
-                    Debug.Log("You blew up and you have "+health+" health left.");
-                
-                    if (health == 0)
+            switch (collision.gameObject.tag)
+            {
+                case "Friendly":
+                    Debug.Log("you're good");
+                    break;
+                case "Fuel":
+                    Debug.Log("Topped up fuel !");
+                    break;
+                case "Finish":
+                    Debug.Log("Congrats you won !");
+                    WinEffect();
+
+                    break;
+                default:
+                    if (!crashToggle)
                     {
                         CrashEffect();
                     }
-                }
-                break;
+                    break;
 
 
 
 
 
+            }
+        }
+        else
+        {
+            return;
         }
     }
 
     void CrashEffect()
     {
-        GetComponent<Movement>().enabled = false;
-        Invoke("ReloadLevel", delay);
+
+        audioData.Stop();
+        audioData.PlayOneShot(crashAudio);
+        crashParticles.Play();
+        if (!won)
+        {
+
+            
+
+            health -= 1;
+            Debug.Log("You blew up and you have " + health + " health left.");
+
+            if (health == 0)
+            {
+                isTransitioning = true;
+                GetComponent<Movement>().enabled = false;
+                Invoke("ReloadLevel", delay);
+            }
+        }
+
+
+        
         
     }
 
     void WinEffect()
     {
+
+        isTransitioning = true;
+        audioData.Stop();
+        successParticles.Play();
+        audioData.PlayOneShot(successAudio);
         won = true;
         GetComponent<Movement>().enabled = false;
         Invoke("LoadNextLevel", delay);
@@ -72,5 +118,21 @@ public class CollisionHandler : MonoBehaviour
         }
 
         SceneManager.LoadScene(nextSceneIndex); // using string pros: can go back to menu or other specific page if needed, use index: since it is integer so can increment and go to next level
+    }
+
+    void DebugKeys()
+    {
+        if (Input.GetKey(KeyCode.L))
+        {
+            LoadNextLevel();
+        }
+        else if(Input.GetKey(KeyCode.R))
+        {
+            ReloadLevel();
+        }
+        else if (Input.GetKey(KeyCode.C))
+        {
+            crashToggle = !crashToggle; //Toggle collision, will flip true to false false to true
+        }
     }
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,6 +6,11 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] float mainThrust = 1000f;
     [SerializeField] float rotateThrust = 100f;
+    [SerializeField] AudioClip mainThrustAudio;
+    [SerializeField] ParticleSystem mainThrustParticles;
+    [SerializeField] ParticleSystem leftThrustParticles;
+    [SerializeField] ParticleSystem rightThrustParticles;
+
     Rigidbody rb;
     AudioSource audioData;
 
@@ -27,38 +32,57 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space)) //avoid using string because naming could be a headache
         {
-            
-     
-            rb.AddRelativeForce(Vector3.up  * mainThrust * Time.deltaTime ); // alternative way to write rb.AddRelativeForce(0, 1, 0)
-
-            if (!audioData.isPlaying)
-            {
-                audioData.Play();
-            }
+            StartThrust();
 
         }
         else
         {
-            audioData.Stop();
+            // issue, it will stop all audio including crash audio
+            StopThrust();
+
+        }
+
+
+    }
+    void RotationProcess()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            RotateLeft();
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            RotateRight();
+
+        }
+        else
+        {
+            StopRotateParticles();
         }
 
 
     }
 
-    void RotationProcess()
+
+    void StartThrust()
     {
-        if (Input.GetKey(KeyCode.A))
+        rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime); // alternative way to write rb.AddRelativeForce(0, 1, 0)
+
+        if (!audioData.isPlaying)
         {
-            RotateRocket(rotateThrust);
-            
+            audioData.PlayOneShot(mainThrustAudio);
         }
 
-        else if (Input.GetKey(KeyCode.D))
+        if (!mainThrustParticles.isPlaying)
         {
-            RotateRocket(-rotateThrust);
-            
+            mainThrustParticles.Play();
         }
-        
+    }
+    void StopThrust()
+    {
+        audioData.Stop();
+        mainThrustParticles.Stop();
     }
 
     void RotateRocket(float rotationValue)
@@ -67,6 +91,37 @@ public class Movement : MonoBehaviour
         transform.Rotate(Vector3.forward * rotationValue * Time.deltaTime);
         rb.freezeRotation = false; // unfreezing rotation so the physics system can take over
 
-        
+
     }
+
+    void RotateLeft()
+    {
+        RotateRocket(rotateThrust);
+
+        if (!leftThrustParticles.isPlaying)
+        {
+            leftThrustParticles.Play();
+        }
+    }
+
+
+    void RotateRight()
+    {
+        RotateRocket(-rotateThrust);
+
+        if (!rightThrustParticles.isPlaying)
+        {
+            rightThrustParticles.Play();
+        }
+    }
+
+    
+
+    void StopRotateParticles()
+    {
+        rightThrustParticles.Stop();
+        leftThrustParticles.Stop();
+    }
+
+    
 }
